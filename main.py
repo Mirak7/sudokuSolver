4543a8eaa124a81766f47a9230698a021f751dbe
p2414115 <bencheikh.a.karim@gmail.com>
2025-04-13 16:33:04 +0200

    implemented a backtracking solver for sudoku

---
+++ main.py
@@ -0,0 +1,6 @@
+from solver import is_empty, check_col, check_neighbor, check_row, solver
+from grids import sudoku_grid, empty_grid
+from pprint import pprint
+
+solver(empty_grid)
+pprint(empty_grid)
--- solver.py
+++ solver.py
@@ -1,10 +1,18 @@
+from pprint import pprint
 # returns the first empty grid case else false if full
+def is_empty(grid) -> bool:
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == 0:
+                return True
+    return False
+
+
 def find_empty(grid):
     for i in range(len(grid)):
         for j in range(len(grid)):
-            if grid[i][j] == ".":
+            if grid[i][j] == 0:
                 return i, j
-    return False
 
 
 # check if grid cell is a valid one row wise
@@ -21,17 +29,37 @@ def check_col(grid, num, col):
         
     return True
 
+# check if grid cell is a valid one box wise
 def check_neighbor(grid, num, row, col):
     row_pos = (row // 3) * 3
     col_pos = (col // 3) * 3
     
-    for i in range(row_pos + 3):
-        for j in range(col_pos + 3):
+    for i in range(row_pos, row_pos + 3):
+        for j in range(col_pos, col_pos + 3):
             if grid[i][j] == num:
                 return False
     
     return True
-
+    
+    
+# backtracking Solver
+def solver(grid):
+    if (not is_empty(grid)):
+        return True
+    
+    row, col = find_empty(grid)
+    for i in range(1, 10):
+        if (check_col(grid, i, col) and check_row(grid, i, row) and check_neighbor(grid, i, row, col)):
+            grid[row][col] = i
+            if solver(grid):
+                return True
+            grid[row][col] = 0
+    
+    return False
+    
+    
+            
+        
 
 grid = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
         [2, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -49,7 +77,7 @@ sudoku_grid = [
     [1, 9, 8, 3, 4, 2, 5, 6, 7],
     
     [8, 5, 9, 7, 6, 1, 4, 2, 3],
-    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [4, 2, 6, 0, 5, 3, 7, 9, 1],
     [7, 1, 3, 9, 2, 4, 8, 5, 6],
     
     [9, 6, 1, 5, 3, 7, 2, 8, 4],
@@ -57,5 +85,16 @@ sudoku_grid = [
     [3, 4, 5, 2, 8, 6, 1, 7, 9]
 ]
 
-print(check_neighbor(sudoku_grid, 2, 1, 1))
-#print(check_valid(sudoku_grid))
+empty_grid = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
