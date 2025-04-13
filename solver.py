3b5d8d2ad2ba4127f08ec7a340bd786cc295d5f5
p2414115 <bencheikh.a.karim@gmail.com>
2025-04-13 14:44:18 +0200

    first commit with check valid functions for sudoku grid

---
+++ solver.py
@@ -0,0 +1,58 @@
+# returns the first empty grid case else false if full
+def find_empty(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == ".":
+                return i, j
+    return False
+
+# check every cell of the grid if valid or not
+def check_valid(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if(not check_col(grid, i, j) or not check_col(grid, i, j)):
+                return i, j
+    return True
+
+# check if grid cell is a valid one row wise
+def check_row(grid, num, row):
+    if num in grid[row]:
+        return False
+    
+    return True
+
+# check if grid cell is a valid one col wise
+def check_col(grid, num, col):
+    if num in [grid[i][col] for i in range(len(grid))]:
+        return False
+        
+    return True
+
+#def check_neighbor(grid, i, j):
+    
+
+grid = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [2, 2, 3, 4, 5, 6, 7, 8, 9],
+        [3, 2, 3, 4, 5, 6, 7, 8, 9],
+        [4, 2, 3, 4, 5, 6, 7, 8, 9],
+        [5, 2, 3, 4, 5, 6, 7, 8, 9],
+        [6, 2, 3, 4, 5, 6, 7, 8, 9],
+        [7, 2, 3, 4, 5, 6, 7, 8, 9],
+        [8, 2, 3, 4, 5, 6, 7, 8, 9],
+        [9, 2, 3, 4, 5, 6, 7, 8, 9]]
+
+sudoku_grid = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+]
+print(check_col(sudoku_grid, 1))
+#print(check_valid(sudoku_grid))
